10d5c2b256d3d5a3991c40a326b1f3272bac1e38
wanggang <410689330@163.com>
2015-12-11 17:34:31 +0800

    修改部分 Bug

--- app/src/main/java/com/qingsongjia/qingsongjia/bean/SchoolDetail.java
+++ app/src/main/java/com/qingsongjia/qingsongjia/bean/SchoolDetail.java
@@ -4,5 +4,170 @@ package com.qingsongjia.qingsongjia.bean;
  * Created by wanggang on 15/12/11.
  */
 public class SchoolDetail {
+    //    "countpepole": 0,
+//            "dri_address": "山西省太原市东岗南路252-30号（南内环街现代女子医院南300米）",
+//            "dri_campus_id": 288,
+//            "dri_file_path": "http://7xlt5l.com1.z0.glb.clouddn.com/1449805758007orzzax9658691",
+//            "dri_map_address": "",
+//            "dri_money": 3600,
+//            "dri_nm": "东风驾校",
+//            "dri_pass": 0,
+//            "dri_place": 0,
+//            "dri_report": "欢迎光临太原东风驾校，东风驾校电话：0351-7683105， 太原东风驾校怎么样，价格贵不贵？东风驾校好不好？ 东风驾校地址：山西省太原市东岗南路252-30号（南内环街现代女子医院南300米）\r\n业务咨询电话 ：0351-7683105，7683195 \r\n1、真正的正规驾校：有自主考场，考试快通过率高；\r\n20-30 年驾龄老师傅，与你分享全路况驾校经验；\r\n2、20 年驾校办学经验； \r\n3、30台注册皮卡，捷达教练车，车况良好、不论刮风下雪，练车时间充足保障； \r\n4、8 人以上团体报名，另有惊喜赠送； \r\n5、1 站式服务，涵盖：学车、落户、体检、审本、审车、违章处理、代驾等； \r\n6、95%年平均通过率，随到随学，循环培训，循环考核，； \r\n7、每期超百人的学员容量，固定紧密的社交平台；",
+//            "dri_sum": 0,
+//            "dri_tel1": "0351-7683195",
+//            "dri_tel2": "0351-7683105",
+//            "dri_time": 0,
+//            "dri_way": "",
+//            "street_view": []
 
+
+    private int countpepole;
+    private String dri_address;
+    private int dri_campus_id;
+    private String dri_file_path;
+    private String dri_map_address;
+    private int dri_money;
+    private String dri_nm;
+    private int dri_pass;
+    private int dri_place;
+    private int dri_sum;
+    private String dri_report;
+
+    private String dri_tel1;
+    private String dri_tel2;
+    private String dri_way;
+    private int dri_time;
+    private String[] street_view;
+
+    public SchoolDetail() {
+    }
+
+    public int getCountpepole() {
+        return countpepole;
+    }
+
+    public void setCountpepole(int countpepole) {
+        this.countpepole = countpepole;
+    }
+
+    public String getDri_address() {
+        return dri_address;
+    }
+
+    public void setDri_address(String dri_address) {
+        this.dri_address = dri_address;
+    }
+
+    public int getDri_campus_id() {
+        return dri_campus_id;
+    }
+
+    public void setDri_campus_id(int dri_campus_id) {
+        this.dri_campus_id = dri_campus_id;
+    }
+
+    public String getDri_file_path() {
+        return dri_file_path;
+    }
+
+    public void setDri_file_path(String dri_file_path) {
+        this.dri_file_path = dri_file_path;
+    }
+
+    public String getDri_map_address() {
+        return dri_map_address;
+    }
+
+    public void setDri_map_address(String dri_map_address) {
+        this.dri_map_address = dri_map_address;
+    }
+
+    public int getDri_money() {
+        return dri_money;
+    }
+
+    public void setDri_money(int dri_money) {
+        this.dri_money = dri_money;
+    }
+
+    public String getDri_nm() {
+        return dri_nm;
+    }
+
+    public void setDri_nm(String dri_nm) {
+        this.dri_nm = dri_nm;
+    }
+
+    public int getDri_pass() {
+        return dri_pass;
+    }
+
+    public void setDri_pass(int dri_pass) {
+        this.dri_pass = dri_pass;
+    }
+
+    public int getDri_place() {
+        return dri_place;
+    }
+
+    public void setDri_place(int dri_place) {
+        this.dri_place = dri_place;
+    }
+
+    public int getDri_sum() {
+        return dri_sum;
+    }
+
+    public void setDri_sum(int dri_sum) {
+        this.dri_sum = dri_sum;
+    }
+
+    public String getDri_report() {
+        return dri_report;
+    }
+
+    public void setDri_report(String dri_report) {
+        this.dri_report = dri_report;
+    }
+
+    public String getDri_tel1() {
+        return dri_tel1;
+    }
+
+    public void setDri_tel1(String dri_tel1) {
+        this.dri_tel1 = dri_tel1;
+    }
+
+    public String getDri_tel2() {
+        return dri_tel2;
+    }
+
+    public void setDri_tel2(String dri_tel2) {
+        this.dri_tel2 = dri_tel2;
+    }
+
+    public String getDri_way() {
+        return dri_way;
+    }
+
+    public void setDri_way(String dri_way) {
+        this.dri_way = dri_way;
+    }
+
+    public int getDri_time() {
+        return dri_time;
+    }
+
+    public void setDri_time(int dri_time) {
+        this.dri_time = dri_time;
+    }
+
+    public String[] getStreet_view() {
+        return street_view;
+    }
+
+    public void setStreet_view(String[] street_view) {
+        this.street_view = street_view;
+    }
 }
--- app/src/main/java/com/qingsongjia/qingsongjia/driverschool/SchoolDetailActivity.java
+++ app/src/main/java/com/qingsongjia/qingsongjia/driverschool/SchoolDetailActivity.java
@@ -1,14 +1,114 @@
 package com.qingsongjia.qingsongjia.driverschool;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.qingsongjia.qingsongjia.R;
+import com.qingsongjia.qingsongjia.bean.SchoolDetail;
 import com.qingsongjia.qingsongjia.utils.NetRequest;
 import com.qingsongjia.qingsongjia.utils.NetUtils;
 import com.wan7451.base.WanActivity;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class SchoolDetailActivity extends WanActivity {
 
+    private void fillData(SchoolDetail detail) {
+        if (detail == null)
+            return;
+        if (!TextUtils.isEmpty(detail.getDri_file_path())) {
+            schoolinfoIvLogo.setImageURI(Uri.parse(detail.getDri_file_path()));
+        }
+        schoolinfoTvName.setText(detail.getDri_nm());
+        schoolinfoTvPrice.setText(detail.getDri_money() + "元");
+        if (!TextUtils.isEmpty(detail.getDri_tel1())
+                && !TextUtils.isEmpty(detail.getDri_tel2())) {
+            schoolinfoTvTel.setText(detail.getDri_tel1() + " " + detail.getDri_tel2());
+        } else if (!TextUtils.isEmpty(detail.getDri_tel1())
+                && TextUtils.isEmpty(detail.getDri_tel2())) {
+            schoolinfoTvTel.setText(detail.getDri_tel1());
+        } else if (TextUtils.isEmpty(detail.getDri_tel1())
+                && !TextUtils.isEmpty(detail.getDri_tel2())) {
+            schoolinfoTvTel.setText(detail.getDri_tel2());
+        } else {
+            schoolinfoTvTel.setText("暂时没有电话");
+        }
+        schoolinfoTvAddr.setText(detail.getDri_address());
+        schoolinfoIvImgsCount.setText(detail.getStreet_view().length+"张");
+//        schoolinfoGvImgs
 
+        schoolinfoTvFen.setText(detail.getDri_sum()+"分");
+        schoolinfoTvRen.setText(detail.getCountpepole()+"人评分");
+
+        schoolinfoTimeRating.setRating(detail.getDri_time());
+        schoolinfoTimeRating.setMax(10);
+        schoolinfoTimeFen.setText(detail.getDri_time()+"分");
+
+        schoolinfoAddrRating.setRating(detail.getDri_place());
+        schoolinfoAddrRating.setMax(10);
+        schoolinfoAddrFen.setText(detail.getDri_place()+"分");
+
+        schoolinfoPassRating.setRating(detail.getDri_pass());
+        schoolinfoPassRating.setMax(10);
+        schoolinfoPassFen.setText(detail.getDri_pass()+"分");
+
+        schoolinfoTvInfo.setText(detail.getDri_report());
+
+//        schoolinfoTvBus.setText(detail.getDri_way());
+    }
+
+
+    @Bind(R.id.schoolinfo_iv_logo)
+    SimpleDraweeView schoolinfoIvLogo;
+    @Bind(R.id.schoolinfo_tv_name)
+    TextView schoolinfoTvName;
+    @Bind(R.id.schoolinfo_tv_price)
+    TextView schoolinfoTvPrice;
+    @Bind(R.id.schoolinfo_layout_price)
+    RelativeLayout schoolinfoLayoutPrice;
+    @Bind(R.id.schoolinfo_tv_tel)
+    TextView schoolinfoTvTel;
+    @Bind(R.id.schoolinfo_tv_addr)
+    TextView schoolinfoTvAddr;
+    @Bind(R.id.schoolinfo_tv_bus)
+    TextView schoolinfoTvBus;
+    @Bind(R.id.schoolinfo_iv_imgsCount)
+    TextView schoolinfoIvImgsCount;
+    @Bind(R.id.schoolinfo_layout_img)
+    RelativeLayout schoolinfoLayoutImg;
+    @Bind(R.id.schoolinfo_gv_imgs)
+    GridView schoolinfoGvImgs;
+    @Bind(R.id.schoolinfo_tv_fen)
+    TextView schoolinfoTvFen;
+    @Bind(R.id.schoolinfo_tv_ren)
+    TextView schoolinfoTvRen;
+    @Bind(R.id.schoolinfo_time_rating)
+    RatingBar schoolinfoTimeRating;
+    @Bind(R.id.schoolinfo_time_fen)
+    TextView schoolinfoTimeFen;
+    @Bind(R.id.schoolinfo_addr_rating)
+    RatingBar schoolinfoAddrRating;
+    @Bind(R.id.schoolinfo_addr_fen)
+    TextView schoolinfoAddrFen;
+    @Bind(R.id.schoolinfo_pass_rating)
+    RatingBar schoolinfoPassRating;
+    @Bind(R.id.schoolinfo_pass_fen)
+    TextView schoolinfoPassFen;
+    @Bind(R.id.schoolinfo_tv_info)
+    TextView schoolinfoTvInfo;
+    @Bind(R.id.schoolinfo_btn_baoming)
+    Button schoolinfoBtnBaoming;
     private int id;
 
     @Override
@@ -21,11 +121,16 @@ public class SchoolDetailActivity extends WanActivity {
     }
 
     private void loadData() {
-        NetRequest.loadSchoolDeatail(getContext(),id, new NetUtils.NetUtilsHandler() {
+        NetRequest.loadSchoolDeatail(getContext(), id, new NetUtils.NetUtilsHandler() {
             @Override
             public void onResponseOK(JSONArray response, int total) {
-
+                if (!TextUtils.equals(response.toJSONString(), "[{}]")) {
+                    String data = response.getString(0);
+                    SchoolDetail detail = JSONObject.parseObject(data, SchoolDetail.class);
+                    fillData(detail);
+                }
             }
+
 
             @Override
             public void onResponseError(String error) {
@@ -34,8 +139,16 @@ public class SchoolDetailActivity extends WanActivity {
         });
     }
 
+
     @Override
     protected int getMainViewLayoutId() {
         return R.layout.activity_school_info;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
